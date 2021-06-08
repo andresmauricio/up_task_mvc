6301696018a27a9386d1566818af79c2e2e37be8
andresmauricio <andresacelas@outlook.com>
2021-06-07 20:25:45 -0500

    implements express-validator

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator/check')
 const router = express.Router()
 const proyectController = require('../controllers/proyectsController')
 
 module.exports = function () {
   router.get('/home', proyectController.proyectHome)
   router.get('/new-proyect', proyectController.formProyect)
-  router.post('/new-proyect', proyectController.newProyect)
+  router.post(
+    '/new-proyect',
+    body('nombre').not().isEmpty().trim().escape(),
+    proyectController.newProyect
+  )
   return router
 }
