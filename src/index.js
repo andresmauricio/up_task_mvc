dfd1765b018751c18155a6dc019aa33117c04754
andresmauricio <andresacelas@outlook.com>
2021-06-08 22:19:02 -0500

    create fuction vardump, and inser locals fuctions use global app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const router = require('./routes')
 const sequelize = require('./config/database')
+const { vardump } = require('./helpers')
 const app = express()
 
 require('./models/Projects')
@@ -10,6 +11,10 @@ sequelize
   .then(() => console.log('Database is connected'))
   .catch(error => console.log('Error to connected database', error))
 
+app.use((req, res, next) => {
+  res.locals.vardump = vardump
+  next()
+})
 app.use(express.static(path.join(__dirname, './public')))
 app.use(express.urlencoded({ extended: true }))
 
