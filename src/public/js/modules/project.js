e74215e7dc33be7e06e50caf03142bbcc89cf23b
andresmauricio <andresacelas@outlook.com>
2021-06-10 22:36:58 -0500

    alert config for delete project

--- src/public/js/modules/project.js
+++ src/public/js/modules/project.js
@@ -2,6 +2,23 @@ import Swal from 'sweetalert2'
 import axios from 'axios'
 
 const buttonDelete = document.getElementById('eliminar-proyecto')
-buttonDelete.addEventListener('click', () => {
-  console.log('delete project')
+buttonDelete?.addEventListener('click', () => {
+  Swal.fire({
+    title: '¿Desear eliminar el proyecto?',
+    text: 'Un proyecto eliminado no se puede recuperar',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, borrar',
+    cancelButtonText: 'No borrar'
+  }).then(result => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Borrado!',
+        'El proyecto se elimino correctamente',
+        'success'
+      ).then(() => (window.location.href = '/home'))
+    }
+  })
 })
