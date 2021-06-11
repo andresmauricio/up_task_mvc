b8938d590ae3dd85ab28baf81c13556d9aaf2810
andresmauricio <andresacelas@outlook.com>
2021-06-10 22:30:01 -0500

    install axios and swal, config bundle.js in index project and import modules to project

---
+++ src/public/js/modules/project.js
@@ -0,0 +1,7 @@
+import Swal from 'sweetalert2'
+import axios from 'axios'
+
+const buttonDelete = document.getElementById('eliminar-proyecto')
+buttonDelete.addEventListener('click', () => {
+  console.log('delete project')
+})
