f624f48d4b9d2ba9f1ad6169cba4a86b4a2effa0
andresmauricio <andresacelas@outlook.com>
2021-06-22 20:43:46 -0500

    create template for send email and juice pack for combinate

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -7,10 +7,15 @@ const config = require('../config/email')
 
 let transport = nodemailer.createTransport(config)
 
+const generateHtml = () => {
+  const html = pug.renderFile(`${__dirname}/../views/emails/recovery-password-email.pug`)
+  return juice(html)
+}
+
 transport.sendMail({
   from: 'UpTask <suport@uptask.com>', // sender address
   to: 'qhb2l7ienk@buy-blog.com', // list of receivers
   subject: 'Hello ', // Subject line
   text: 'Password Reset', // plain text body
-  html: '<b>Hello world</b>' // html body
+  html: generateHtml() // html body
 })
