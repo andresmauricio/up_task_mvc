3abf7d10ff1f5a9fe6bc229a0223c08abb266fa0
andresmauricio <andresacelas@outlook.com>
2021-07-22 12:10:31 -0500

    config module to enviroments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import app from './server';
+import config from './config/constanst';
 
 async function main() {
-  await app.listen(3000);
+  await app.listen(config.port);
 }
 
 main().then(() => console.log('server running'));
