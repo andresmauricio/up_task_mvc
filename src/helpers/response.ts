ad87a8c057a9272ff594721354a121a784cdc5a4
andresmauricio <andresacelas@outlook.com>
2021-07-31 21:14:33 -0500

    validate field in create user

--- src/helpers/response.ts
+++ src/helpers/response.ts
@@ -1,8 +1,18 @@
+import { validationResult } from "express-validator";
+
 export const success = (req, res, status = 200, data) => {
   return res.status(status).json({ status, error: null, data });
 };
 
 export const errorResponse = (req, res, status = 500, data) => {
   return res.status(status).json({ status, error: true, data });
+};
+
+export const errorValidations = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return errorResponse(req, res, 400, errors.array());
+  }
+  next();
 };
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import passport from 'passport';
 import * as user from '../controllers/user.controller';
 import { isSuperAdmin } from '../middleware/roles';
+import { body, check } from 'express-validator';
+import { errorValidations } from '../helpers/response';
 
 const router = Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), user.list);
-router.post('/', user.create);
+router.post(
+  '/',
+  body('email').isEmail().normalizeEmail(),
+  body('password').notEmpty(),
+  errorValidations,
+  user.create
+);
 router.delete(
   '/:id',
   [passport.authenticate('jwt', { session: false }), isSuperAdmin],
