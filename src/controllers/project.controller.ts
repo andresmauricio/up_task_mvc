811187e1d9e923944e7b100a04fd92175ba21cb1
andresmauricio <andresacelas@outlook.com>
2021-08-06 16:22:38 -0500

    implemenst module

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from 'express';
 import Project from '../models/Project';
+import { v4 as uuidv4 } from 'uuid';
+import slug from 'slug';
+import { success } from '../helpers/response';
 
 export const list = async (req: Request, res: Response) => {
   const { id } = req.params;
   const projects = await Project.findAll({
     where: { userId: id }
   });
-  res.json({ projects });
+  success(req, res, 201, projects);
+};
+
+export const create = async (req: Request, res: Response) => {
+  const id = uuidv4();
+  const { name, userId } = req.body;
+  const url = slug(`${name}-${id}`);
+  const newproject = {
+    id,
+    name,
+    url,
+    userId,
+    is_finish: false
+  };
+  const project = await Project.create(newproject);
+  success(req, res, 201, project);
 };
--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -4,6 +4,7 @@ import * as projects from '../controllers/project.controller';
 const router = Router();
 
 router.get('/list/:id', projects.list);
+router.post('/create', projects.create);
 
 
 export default router;
