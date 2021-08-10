3b81142b8812dbe91ea4ca385961ebff1410464d
andresmauricio <andresacelas@outlook.com>
2021-08-09 21:54:35 -0500

    update state taks and name

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -2,12 +2,14 @@ import { Request, Response } from 'express';
 import Task from '../models/Task';
 import { v4 as uuidv4 } from 'uuid';
 import { errorResponse, success } from '../helpers/response';
+import Project from '../models/Project';
 
 export const list = async (req: Request, res: Response) => {
   const { id_project } = req.params;
   try {
     const tasks = await Task.findAll({
-      where: { projectId: id_project }
+      where: { projectId: id_project },
+      include: { model: Project }
     });
     success(req, res, 201, tasks);
   } catch (error) {
@@ -26,6 +28,19 @@ export const create = async (req: Request, res: Response) => {
   try {
     const task = await Task.create(newtask);
     success(req, res, 201, task);
+  } catch (error) {
+    errorResponse(req, res, 404, null);
+  }
+};
+
+export const update = async (req: Request, res: Response) => {
+  const { name, state, id } = req.body;
+  try {
+    const task: any = await Task.findOne({ where: { id } });
+    task.name = name;
+    task.state = state ? true : false;
+    const taskUpdated = await task.save();
+    success(req, res, 200, taskUpdated);
   } catch (error) {
     errorResponse(req, res, 404, null);
   }
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get('/list/:id_project', tasks.list);
 router.post('/create', tasks.create);
+router.put('/update', tasks.update);
 
 
 export default router;
