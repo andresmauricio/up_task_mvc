0ff65ef68b1632cea9a2d81d1f8ad9295af179eb
andresmauricio <andresacelas@outlook.com>
2021-08-09 21:27:07 -0500

    add try catch task controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
 import Task from '../models/Task';
 import { v4 as uuidv4 } from 'uuid';
-import { success } from '../helpers/response';
+import { errorResponse, success } from '../helpers/response';
 
 export const list = async (req: Request, res: Response) => {
   const { id_project } = req.params;
-  const tasks = await Task.findAll({
-    where: { projectId: id_project }
-  });
-  success(req, res, 201, tasks);
+  try {
+    const tasks = await Task.findAll({
+      where: { projectId: id_project }
+    });
+    success(req, res, 201, tasks);
+  } catch (error) {
+    errorResponse(req, res, 404, null);
+  }
 };
 
 export const create = async (req: Request, res: Response) => {
@@ -17,8 +21,12 @@ export const create = async (req: Request, res: Response) => {
   const newtask = {
     id,
     name,
-    projectId,
+    projectId
   };
-  const task = await Task.create(newtask);
-  success(req, res, 201, task);
+  try {
+    const task = await Task.create(newtask);
+    success(req, res, 201, task);
+  } catch (error) {
+    errorResponse(req, res, 404, null);
+  }
 };
